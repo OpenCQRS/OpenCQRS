a2749d9b81f215ecc06ab709b42dd8159ae0833d
lucabriguglia <luca.briguglia@outlook.com>
2025-08-26 18:49:38 +0100

    Add `CommandResponse` class and extend README with additional Entity Framework Core provider features

---
+++ src/OpenCqrs/Commands/CommandResponse.cs
@@ -0,0 +1,29 @@
+﻿using OpenCqrs.Notifications;
+
+namespace OpenCqrs.Commands;
+
+public class CommandResponse
+{
+    public IEnumerable<INotification>? Notifications { get; set; }
+    public object? Result { get; set; }
+
+    public CommandResponse()
+    {
+    }
+
+    public CommandResponse(INotification notification, object? result = null)
+    {
+        Notifications = new List<INotification>
+        {
+            notification
+        };
+
+        Result = result;
+    }
+    
+    public CommandResponse(IEnumerable<INotification> notifications, object? result = null)
+    {
+        Notifications = notifications;
+        Result = result;
+    }
+}
