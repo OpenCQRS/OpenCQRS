47fb594f7bf435871f8790a3e2018a3e9a2c6652
lucabriguglia <luca.briguglia@outlook.com>
2025-08-31 17:39:12 +0100

    Add `GetLatestEventSequenceTests` to validate event sequence retrieval functionality.

---
+++ tests/OpenCQRS.EventSourcing.Store.Cosmos.Tests/Features/GetLatestEventSequenceTests.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using OpenCQRS.EventSourcing.Store.Cosmos.Tests.Models.Aggregates;
+using OpenCQRS.EventSourcing.Store.Cosmos.Tests.Models.Streams;
+using Xunit;
+
+namespace OpenCQRS.EventSourcing.Store.Cosmos.Tests.Features;
+
+public class GetLatestEventSequenceTests : TestBase
+{
+    [Fact]
+    public async Task GivenNoDomainEventsSaved_TheLatestEventSequenceReturnedIsZero()
+    {
+        var id = Guid.NewGuid().ToString();
+        var streamId = new TestStreamId(id);
+
+        var latestEventSequence = await DomainService.GetLatestEventSequence(streamId);
+
+        latestEventSequence.Value.Should().Be(0);
+    }
+
+    [Fact]
+    public async Task GivenMultipleDomainEventsSaved_TheLatestEventSequenceIsReturned()
+    {
+        var id = Guid.NewGuid().ToString();
+        var streamId = new TestStreamId(id);
+        var aggregateId = new TestAggregate1Id(id);
+        var aggregate = new TestAggregate1(id, "Test Name", "Test Description");
+        aggregate.Update("Updated Name", "Updated Description");
+        aggregate.Update("Updated Name 2", "Updated Description 2");
+        aggregate.Update("Updated Name 3", "Updated Description 3");
+        
+        await DomainService.SaveAggregate(streamId, aggregateId, aggregate, expectedEventSequence: 0);
+        var latestEventSequence = await DomainService.GetLatestEventSequence(streamId);
+
+        latestEventSequence.Value.Should().Be(4);
+    }
+}
