789ee33018db85a1b4cd8985781f0ed9a7856c9f
lucabriguglia <luca.briguglia@outlook.com>
2025-09-02 19:16:40 +0100

    Add ServiceBus and RabbitMQ messaging providers to support queue and topic messaging:
    - Created `ServiceBusMessagingProvider` and `RabbitMqMessagingProvider` implementing `IMessagingProvider`.
    - Project files and solution updated for the new providers with .NET 9.0 target framework.
    - Included project references to `OpenCqrs`.

---
+++ src/OpenCQRS.Messaging.ServiceBus/ServiceBusMessagingProvider.cs
@@ -0,0 +1,17 @@
+﻿using OpenCqrs.Messaging;
+using OpenCqrs.Results;
+
+namespace OpenCQRS.Messaging.ServiceBus;
+
+public class ServiceBusMessagingProvider : IMessagingProvider
+{
+    public Task<Result> SendQueueMessage<TMessage>(TMessage message) where TMessage : IQueueMessage
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Result> SendTopicMessage<TMessage>(TMessage message) where TMessage : ITopicMessage
+    {
+        throw new NotImplementedException();
+    }
+}
