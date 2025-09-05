55237abd47aae09463120c3da6e159f5aa55f9f8
lucabriguglia <luca.briguglia@outlook.com>
2025-09-05 07:33:42 +0100

    Add descriptive messages to `NotImplementedException` in default providers

--- src/OpenCqrs/Messaging/DefaultMessagingProvider.cs
+++ src/OpenCqrs/Messaging/DefaultMessagingProvider.cs
@@ -4,13 +4,15 @@ namespace OpenCqrs.Messaging;
 
 public class DefaultMessagingProvider : IMessagingProvider
 {
+    private static string NotImplementedMessage => "No messaging provider has been configured. Please configure a messaging provider such as Service Bus or RabbitMQ.";
+    
     public Task<Result> SendQueueMessage<TMessage>(TMessage message, CancellationToken cancellationToken = default) where TMessage : IQueueMessage
     {
-        throw new NotImplementedException();
+        throw new NotImplementedException(NotImplementedMessage);
     }
 
     public Task<Result> SendTopicMessage<TMessage>(TMessage message, CancellationToken cancellationToken = default) where TMessage : ITopicMessage
     {
-        throw new NotImplementedException();
+        throw new NotImplementedException(NotImplementedMessage);
     }
 }
--- src/OpenCqrs/Validation/DefaultValidationProvider.cs
+++ src/OpenCqrs/Validation/DefaultValidationProvider.cs
@@ -4,13 +4,15 @@ namespace OpenCqrs.Validation;
 
 public class DefaultValidationProvider : IValidationProvider
 {
+    private static string NotImplementedMessage => "No validation provider has been configured. Please configure a validation provider such as FluentValidation.";
+
     public Task<ValidationResponse> Validate<TCommand>(TCommand command, CancellationToken cancellationToken = default) where TCommand : ICommand
     {
-        throw new NotImplementedException();
+        throw new NotImplementedException(NotImplementedMessage);
     }
 
     public Task<ValidationResponse> Validate<TResponse>(ICommand<TResponse> command, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        throw new NotImplementedException(NotImplementedMessage);
     }
 }
